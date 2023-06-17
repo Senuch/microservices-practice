24d2fec2a91ce0973393672416d1d1704bc62742
senuch <s.uzairtariq@gmail.com>
2023-06-17 14:57:58 +0500

    Configured JPA and persisted customers to database

--- customer/src/main/java/com/jerry/customer/CustomerService.java
+++ customer/src/main/java/com/jerry/customer/CustomerService.java
@@ -3,7 +3,7 @@ package com.jerry.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -11,5 +11,6 @@ public record CustomerService() {
                 .email(request.email())
                 .build();
 
+        customerRepository.save(customer);
     }
 }
