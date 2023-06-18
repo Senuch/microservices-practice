9692f3f23768be846f5ef1a2ba48b106dcbb8409
senuch <s.uzairtariq@gmail.com>
2023-06-18 21:49:16 +0500

    Service Resolution via Eureka Discovery

--- customer/src/main/java/com/jerry/customer/CustomerService.java
+++ customer/src/main/java/com/jerry/customer/CustomerService.java
@@ -14,7 +14,7 @@ public class CustomerService {
         Customer customer = Customer.builder().firstName(request.firstName()).lastName(request.lastName()).email(request.email()).build();
         customerRepository.saveAndFlush(customer);
 
-        FraudCheckResponse response = restTemplate.getForObject("http://localhost:8081/api/v1/fraud-check/{customerId}", FraudCheckResponse.class, customer.getId());
+        FraudCheckResponse response = restTemplate.getForObject("http://FRAUD/api/v1/fraud-check/{customerId}", FraudCheckResponse.class, customer.getId());
         if (response != null && response.isFraudster()) {
             throw new IllegalStateException("Fraudster");
         }
