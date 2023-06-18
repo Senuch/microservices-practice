674d411cbb2298caa9de998e7c81a8723bd84a3b
senuch <s.uzairtariq@gmail.com>
2023-06-18 16:18:05 +0500

    Added Check For Fraud on Customer Registration

--- customer/src/main/java/com/jerry/customer/CustomerService.java
+++ customer/src/main/java/com/jerry/customer/CustomerService.java
@@ -2,19 +2,21 @@ package com.jerry.customer;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
 public class CustomerService {
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
     public void registerCustomer(CustomerRegistrationRequest request) {
-        Customer customer = Customer.builder()
-                .firstName(request.firstName())
-                .lastName(request.lastName())
-                .email(request.email())
-                .build();
+        Customer customer = Customer.builder().firstName(request.firstName()).lastName(request.lastName()).email(request.email()).build();
+        customerRepository.saveAndFlush(customer);
 
-        customerRepository.save(customer);
+        FraudCheckResponse response = restTemplate.getForObject("http://localhost:8081/api/v1/fraud-check/{customerId}", FraudCheckResponse.class, customer.getId());
+        if (response != null && response.isFraudster()) {
+            throw new IllegalStateException("Fraudster");
+        }
     }
 }
--- fraud/src/main/java/com/jerry/fraud/FraudController.java
+++ fraud/src/main/java/com/jerry/fraud/FraudController.java
@@ -1,8 +1,10 @@
 package com.jerry.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
@@ -12,7 +14,7 @@ public class FraudController {
     @GetMapping(path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId) {
         boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
-
+        log.info("fraud check request for customer {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
