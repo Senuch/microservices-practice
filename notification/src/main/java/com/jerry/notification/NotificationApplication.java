3d03c9954457dfa8d327ee94fc542f02bf5e2b8c
senuch <s.uzairtariq@gmail.com>
2023-07-15 13:13:19 +0500

    COnfigured Notification Module with AMQP

--- notification/src/main/java/com/jerry/notification/NotificationApplication.java
+++ notification/src/main/java/com/jerry/notification/NotificationApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.jerry.notification",
+                "com.jerry.amqp"
+        }
+)
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
